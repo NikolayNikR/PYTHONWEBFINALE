3990b7519121868541f7db7d82b2fca3834f1bd6
NikolayNikR <kolechka10.10@mail.ru>
2025-05-06 19:00:40 +0300

    Add files via upload

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,27 @@
+import os
+from idlelib.editor import keynames
+
 from sqlalchemy.sql.functions import random
 import json
 
 from app import app
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, Flask, redirect, url_for, request
 import random
 
-import sqlalchemy as sa
-import sqlalchemy.orm as so
-from app.models import User
 
-from flask_login import current_user, login_user, logout_user, login_required
+def save_rooms():
+    with open(app.config['ROOMS_FILE'], 'w') as f:
+        json.dump(rooms, f)
+
+
+def load_rooms() -> object:
+    if os.path.exists(app.config['ROOMS_FILE']):
+        with open(app.config['ROOMS_FILE'], 'r') as f:
+            return json.load(f)
+    return {}
+
+
+rooms = load_rooms()
 
 
 def code():
@@ -17,32 +29,35 @@ def code():
     return random_room
 
 
-rooms = []
-
-
 @app.route('/index')
 @app.route('/')
 def index():
-    return redirect("menu.html")
+    return render_template("menu.html")
 
 
-@app.route('/createrooms')
-def game():
-    game_room = {'key': code(),
-                 'players': '',
-                 'board': [
-                     ['', '', ''],
-                     ['', '', ''],
-                     ['', '', '']
-                 ]}
-
-    rooms.append(game_room)
-
-    with open("trade.json", "w", encoding="utf-8") as json_file:
-        json.dump(rooms, json_file, ensure_ascii=False)
-
-    print(rooms)
-    return render_template("krestikiNoliki.html")
+@app.route('/create', methods=['GET', 'POST'])
+def creategame():
+    if request.method == 'POST':
+        key = code()
+        rooms[key] = {'players': {},
+                      'board': [
+                          ['', '', ''],
+                          ['', '', ''],
+                          ['', '', '']
+                      ]}
+        save_rooms()
+        return redirect(url_for('room', room_code=key))
 
 
+@app.route('/join', methods=['POST'])
+def join_room():
+    room_code = request.form['room_code']
+    if room_code in rooms:
+        return redirect(url_for('room', room_code=key))
 
+
+@app.route('/room/<room_code>')
+def room(room_code):
+    if room_code not in rooms:
+        return "Комната не найдена", 404
+    return render_template('krestikiNoliki.html', room_code=room_code)
--- routes.py
+++ routes.py
@@ -1,4 +1,5 @@
 import os
+from idlelib.editor import keynames
 
 from sqlalchemy.sql.functions import random
 import json
@@ -8,16 +9,9 @@ from flask import render_template, Flask, redirect, url_for, request
 import random
 
 
-
-def code():
-    random_room = random.randint(10000, 99999)
-    return random_room
-
-
 def save_rooms():
     with open(app.config['ROOMS_FILE'], 'w') as f:
         json.dump(rooms, f)
-
 
 
 def load_rooms() -> object:
@@ -30,6 +24,11 @@ def load_rooms() -> object:
 rooms = load_rooms()
 
 
+def code():
+    random_room = random.randint(10000, 99999)
+    return random_room
+
+
 @app.route('/index')
 @app.route('/')
 def index():
@@ -39,14 +38,26 @@ def index():
 @app.route('/create', methods=['GET', 'POST'])
 def creategame():
     if request.method == 'POST':
-        room_code = code()
-        rooms[room_code] = {'key': code(),
-                            'players': {},
-                            'board': [
-                                ['', '', ''],
-                                ['', '', ''],
-                                ['', '', '']
-                            ]}
+        key = code()
+        rooms[key] = {'players': {},
+                      'board': [
+                          ['', '', ''],
+                          ['', '', ''],
+                          ['', '', '']
+                      ]}
         save_rooms()
+        return redirect(url_for('room', room_code=key))
 
-    return render_template('krestikiNoliki.html')
+
+@app.route('/join', methods=['POST'])
+def join_room():
+    room_code = request.form['room_code']
+    if room_code in rooms:
+        return redirect(url_for('room', room_code=key))
+
+
+@app.route('/room/<room_code>')
+def room(room_code):
+    if room_code not in rooms:
+        return "Комната не найдена", 404
+    return render_template('krestikiNoliki.html', room_code=room_code)
